709201d2719cd17939ff7d7d08ca694b00e880ba
Leon Gauweiler <leon.gauweiler@gmail.com>
2023-10-29 20:56:19 +0100

    added: addUser

--- components/users/add-user.tsx
+++ components/users/add-user.tsx
@@ -1,17 +1,104 @@
 import {Button, Divider, Input, Modal, Text} from '@nextui-org/react';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Flex} from '../styles/flex';
+import api from "../../app/api";
+import Select from "react-select";
 
 export const AddUser = () => {
-   const [visible, setVisible] = React.useState(false);
-   const handler = () => setVisible(true);
+    const customStyles = {
+        control: (provided: any) => ({
+            ...provided,
+            border: '1px solid #ccc',
+            width: '100%',
+            height: '48px',
+            fontSize: '1.125rem',
+            background: '#333'
+        }),
+        singleValue: (provided: any) => ({
+            ...provided,
+            color: 'white'
+        }),
+        menu: (provided: any) => ({
+            ...provided,
+            background: '#333',
+        }),
+        option: (provided: any, state: { isSelected: any; isFocused: any; }) => ({
+            ...provided,
+            color: state.isSelected ? 'black' : 'white',
+            background: state.isFocused ? '#555' : '#444',  // Hintergrund der Optionen angepasst
+            '&:hover': {
+                background: '#666'  // Hintergrund der Optionen beim Überfahren mit der Maus
+            }
+        })
+    };
 
-   const closeHandler = () => {
+
+
+    const [visible, setVisible] = React.useState(false);
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [group, setGroup] = React.useState({ value: 2, label: "Default"});
+    const [groups, setGroups] = React.useState([]);
+    const [selectGroups, setSelectGroups] = React.useState([]);
+
+
+    const handler = () => setVisible(true);
+
+    const closeHandler = () => {
       setVisible(false);
       console.log('closed');
-   };
+    };
 
-   return (
+    const handleSubmit = async () => {
+        try {
+            const body = {
+                name: name,
+                email: email,
+                password: password,
+                group: group.value
+            };
+            const response = await api.post("/v1/account/user", body);
+            if (response.status === 201) {
+                closeHandler();
+            } else {
+                console.log(response.status, response.statusText);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const token = localStorage.getItem('token');
+            if (token) {
+                console.log(token);
+                api.get('/v1/account/group/list', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                    .then(groupResponse => {
+                        console.log(groupResponse.status);
+                        setGroups(groupResponse.data);
+                        setSelectGroups(
+                            groupResponse.data.map((group: { id: number; name: string; }) => ({
+                                value: group.id,
+                                label: group.name
+                            }))
+                        );
+                        console.log(groupResponse);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            }
+        }
+    }, []);
+
+
+    return (
       <div>
          <Button auto onClick={handler}>
             Add User
@@ -38,88 +125,59 @@ export const AddUser = () => {
                      '@lg': {flexWrap: 'nowrap', gap: '$12'},
                   }}
                >
-                  <Flex
-                     css={{
-                        'gap': '$10',
-                        'flexWrap': 'wrap',
-                        '@lg': {flexWrap: 'nowrap'},
-                     }}
-                  >
-                     <Input
-                        label="First Name"
-                        bordered
-                        clearable
-                        fullWidth
-                        size="lg"
-                        placeholder="First Name"
-                     />
-                     <Input
-                        label="Last Name"
-                        clearable
-                        bordered
-                        fullWidth
-                        size="lg"
-                        placeholder="Last Name"
-                     />
-                  </Flex>
-
-                  <Flex
-                     css={{
-                        'gap': '$10',
-                        'flexWrap': 'wrap',
-                        '@lg': {flexWrap: 'nowrap'},
-                     }}
-                  >
-                     <Input
-                        label="Email"
-                        clearable
-                        bordered
-                        fullWidth
-                        size="lg"
-                        placeholder="Email"
-                     />
-                     <Input
-                        label="Phone Number"
-                        clearable
-                        bordered
-                        fullWidth
-                        size="lg"
-                        placeholder="Phone Number"
-                     />
-                  </Flex>
-                  <Flex
-                     css={{
-                        'gap': '$10',
-                        'flexWrap': 'wrap',
-                        '@lg': {flexWrap: 'nowrap'},
-                     }}
-                  >
-                     <Input
-                        label="Department"
-                        clearable
-                        bordered
-                        fullWidth
-                        size="lg"
-                        placeholder="Department"
-                     />
-                     <Input
-                        label="Company"
-                        clearable
-                        bordered
-                        fullWidth
-                        size="lg"
-                        placeholder="Company"
-                     />
-                  </Flex>
+                   <Input
+                       label="Name"
+                       bordered
+                       clearable
+                       fullWidth
+                       size="lg"
+                       placeholder="Name"
+                       value={name}
+                       onChange={(e) => { setName(e.target.value) }}
+                   />
+                   <Input
+                       label="Email"
+                       clearable
+                       bordered
+                       fullWidth
+                       size="lg"
+                       placeholder="Email"
+                       value={email}
+                       onChange={(e) => { setEmail(e.target.value) }}
+                   />
+                   <Input
+                       label="Password"
+                       type="password"
+                       clearable
+                       bordered
+                       fullWidth
+                       size="lg"
+                       placeholder="Password"
+                       value={password}
+                       onChange={(e) => { setPassword(e.target.value) }}
+                   />
+                   <Select
+                       defaultValue={group}
+                       styles={customStyles}
+                       isClearable={true}
+                       placeholder="Group"
+                       value={group}
+                       options={selectGroups}
+                       onChange={(e) => {
+                           if (e) {
+                               setGroup(e);
+                           }
+                       }}
+                   />
                </Flex>
             </Modal.Body>
             <Divider css={{my: '$5'}} />
             <Modal.Footer>
-               <Button auto onClick={closeHandler}>
+               <Button auto onClick={handleSubmit}>
                   Add User
                </Button>
             </Modal.Footer>
          </Modal>
       </div>
-   );
+    );
 };
